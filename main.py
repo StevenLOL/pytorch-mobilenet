e3db8afb07b898945841d59c690032ddb607d5b5
xiaohang <xiaohang@ainirobot.com>
2017-04-19 17:38:29 +0800

    fix a problem in main.py, version 0.1

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ class Net(nn.Module):
 
         def conv_bn(inp, oup, stride):
             return nn.Sequential(
-                nn.Conv2d(inp, oup, stride, 2, 1, bias=False),
+                nn.Conv2d(inp, oup, 3, stride, 1, bias=False),
                 nn.BatchNorm2d(oup),
                 nn.ReLU(inplace=True)
             )
